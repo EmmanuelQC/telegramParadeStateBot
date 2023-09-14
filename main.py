59af16b0e1b86222d38d79e94c6bcbf010eb31ac
Emmanuel Carter <60098681+EmmanuelQC@users.noreply.github.com>
2023-09-14 20:45:20 +0800

    Update with automatic daily messages and polls

--- main.py
+++ main.py
@@ -16,27 +16,18 @@ import random
 TOKEN: Final = '6422765606:AAGBvW2RUfgb2yM0JIf_nTjiPs8m0_f6vgU'
 BOT_USERNAME: Final = '@paradeStateSurvey_bot'
 
-# u = Updater(TOKEN, True)
-# j = u.job_queue
-
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello, this is the Parade State Bot')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('I am Parade State Bot and will account for the parade state everyday to serve my country!')
+    await update.message.reply_text('I am Parade State Bot and will account for the parade state everyday to serve my country! Use /parade to test the full polling of the bot.')
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello, this is the Parade State Bot')
-
-async def send(context: ContextTypes.DEFAULT_TYPE) -> None:
-    job = context.job
-    await context.bot.send_message(job.chat_id, text=f"Taking ParadeState now")
-
-async def scheduled(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def scheduled_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_message.chat_id
     """ Running on Mon, Tue, Wed, Thu, Fri = tuple(range(5)) at 10:00 UTC time (6 pm SGT) """
     t = datetime.time(hour=10, minute=00, second=00)
-    context.job_queue.run_daily(paradeState_call, t, days=tuple(range(5)), data=None, name=None, chat_id=chat_id)
+    # TODO test run daily - running it from sunday to thursday
+    context.job_queue.run_daily(paradeState_call, t, days=tuple(0,1,2,3,4), data=None, name=None, chat_id=chat_id)
 
     # test running paradeState after 5 second
     #context.job_queue.run_once(paradeState_call, 5, chat_id=chat_id)
@@ -44,44 +35,60 @@ async def scheduled(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def paradeState_call(context: ContextTypes.DEFAULT_TYPE) -> None:
     job = context.job
 
-    today = str(date.today())
+    today = date.today
+
+    tommorrow = today + datetime.timedelta(days=1)
 
     # array of options for parade state poll
-    reports = ["PRESENT", "RSO", "AM LEAVE", "PM LEAVE", "MC", "DUTY", "OTHERS"]
+    reports = ["PRESENT", "RSO", "RSI", "LEAVE (AM/PM)", "OFF (AM/PM)", "DUTY", "MA", "OTHERS"]
 
     # array of branches in Base HQ
     branches = ['S1 🫂', 'S3 🔫', 'S4 💸']
 
-    for branch in branches:
-        message = await context.bot.send_poll(
+    message = await context.bot.send_poll(
         job.chat_id,
-        "📋 Parade State for " + branch + ' ' + today,
+        "📋 Parade State for " + branches[2] + ' ' + str(tommorrow),
         reports,
         is_anonymous=False,
         allows_multiple_answers=False,
-    )
+     )
+
+    """Sends polls for all branches in SBC"""
+    # for branch in branches:
+    #     message = await context.bot.send_poll(
+    #     job.chat_id,
+    #     "📋 Parade State for " + branch + ' ' + today,
+    #     reports,
+    #     is_anonymous=False,
+    #     allows_multiple_answers=False,
+    # )
 
 async def paradeState_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # TODO: make pool repeat 3 times for all branches, allow undoing selection, add automatic scheduling to run poll everyday, deploy to website server
-    today = str(date.today())
+    # TODO: deploy to website server and create command to print the status of the parade state
+    today = date.today()
+
+    tommorrow = today + datetime.timedelta(days=1)
 
     # array of options for parade state poll
-    reports = ["PRESENT", "RSO", "AM LEAVE", "PM LEAVE", "MC", "DUTY", "OTHERS"]
+    reports = ["PRESENT", "RSO", "RSI", "LEAVE (AM/PM)", "OFF (AM/PM)", "DUTY", "MA", "OTHERS"]
 
     # array of branches in Base HQ
     branches = ['S1 🫂', 'S3 🔫', 'S4 💸']
     for branch in branches:
         message = await context.bot.send_poll(
         update.effective_chat.id,
-        "📋 Parade State for " + branch + ' ' + today,
+        "📋 Parade State for " + branch + ' ' + str(tommorrow),
         reports,
         is_anonymous=False,
         allows_multiple_answers=False,
     )
 
+# Handling all other messages
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_type:str = update.message.chat.type
     text: str = update.message.text
+
+    job = context.job
 
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
 
@@ -108,12 +115,11 @@ if __name__ == '__main__':
     # Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('custom', custom_command))
     app.add_handler(CommandHandler('parade', paradeState_command))
-    app.add_handler(CommandHandler('schedule', scheduled))
+    app.add_handler(CommandHandler('scheduled', scheduled_command))
 
     # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    # app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
     # error handling
     app.add_error_handler(error)
